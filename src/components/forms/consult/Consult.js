42caaf2cc56f194c95a02982bb5b2808b662a338
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-09-22 10:34:10 -0500

    return the right form based on siteData

--- src/components/forms/consult/Consult.js
+++ src/components/forms/consult/Consult.js
@@ -1,34 +1,11 @@
 import React from "react"
-import stores from '../../../data/stores.json'
 import Multiple from "./Multiple"
 import Single from "./Single"
 import NotOpen from "./NotOpen"
 
 function Consult({siteData}) {
-  let city
-  city = stores.locations.find(element => {
-    return element.stores.some(findLocation)
-  }).stores.find(findLocation)
-
-  function findLocation(elem) {
-    return elem.city === siteData.city
-  }
-  
-  // For cities with multiple locations
-  if(city.locations.length > 1) {
-    siteData.multiple = true
-    // if any store is Open 
-    let isOpen = city.locations.find(elem => {
-      return elem.open === true
-    })
-    if(isOpen) return <Multiple siteData={siteData} />
-  }
-  // For cities with single location
-  else if(city.locations.length === 1) {
-    siteData.multiple = false
-    if(city.locations[0].open) return <Single siteData={siteData} />
-  }
-
-  return <NotOpen siteData={siteData} />
+  if(siteData.multiple && siteData.open) return <Multiple siteData={siteData} />
+  else if(!siteData.multiple && siteData.open) return <Single siteData={siteData} />
+  else return <NotOpen siteData={siteData} />
 }
 export default Consult
