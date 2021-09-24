e69280d71a4f4027a5b9fdc936ac95cd28dbd3f8
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-09-23 19:56:20 -0500

    Create siteData object

---
+++ src/functions/general/getSiteData.js
@@ -0,0 +1,31 @@
+import stores from '../../data/stores.json'
+import isMultiple from './isMultiple'
+
+// To be called in the Layout Comp
+// Returns siteData obj
+function getSiteData(url, state) {
+ 
+  // 1. Find the matching state from the stores list
+  let currentState = stores.locations.find(elem => elem.state === state)
+  // 2. Find the current city in that state
+  let currentCity = currentState.stores.find(city => city.url === url.toLowerCase())
+
+  // 3. Create a siteData object
+  const siteData = {
+    "url": `https://milanlaser${currentCity.url.toLowerCase()}.com/`,
+    "state": currentState.state,
+    "stateShort": currentState.stateShort,
+    "city": currentCity.city
+  }
+  // if the city is a single location city, add a salesforceValue prop
+  if(currentCity.locations.length === 1) {
+    siteData.salesforceValue = currentCity.locations[0].salesforceValue
+  }
+
+  // Check if the site has multiple locations & if Open
+  isMultiple(siteData)
+  
+  return siteData
+}
+
+export default getSiteData
