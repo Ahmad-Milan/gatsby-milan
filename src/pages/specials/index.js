4abbf771356939b0b50a267025f3117ef7bdc21b
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-09-27 13:35:16 -0500

    Customm Quote link added

--- src/pages/specials/index.js
+++ src/pages/specials/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Layout from '../../templates/Layout'
+import { Link } from 'gatsby'
 
 function index() {
   return (
-    <div>
-      Specials Page
-    </div>
+    <Layout>
+      <div className="mt-5 pt-5"></div>
+      <h1>Specials Page</h1>
+      
+      <Link to="/specials/estimate/">Customm Quote</Link>
+    </Layout>
   )
 }
 
