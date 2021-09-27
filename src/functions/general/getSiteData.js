5a99841ed5754d461cced97ddfa67fa891c362d9
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-09-27 13:37:11 -0500

    moving the logic to setSiteData

--- src/functions/general/getSiteData.js
+++ src/functions/general/getSiteData.js
@@ -1,30 +1,8 @@
-import stores from '../../data/stores.json'
-import isMultiple from './isMultiple'
+import setSiteData from './setSiteData'
 
-// To be called in the Layout Comp
-// Returns siteData obj
-function getSiteData(pathname, state) {
- 
-  // 1. Find the matching state from the stores list
-  let currentState = stores.locations.find(elem => elem.state === state)
-  // 2. Find the current city in that state
-  let currentCity = currentState.stores.find(city => city.pathname === pathname.toLowerCase())
+const siteData = setSiteData('Phoenix', 'Arizona')
 
-  // 3. Create a siteData object
-  const siteData = {
-    "origin": `https://milanlaser${currentCity.pathname.toLowerCase()}.com/`,
-    "state": currentState.state,
-    "stateShort": currentState.stateShort,
-    "city": currentCity.city
-  }
-  // if the city is a single location city, add a salesforceValue prop
-  if(currentCity.locations.length === 1) {
-    siteData.salesforceValue = currentCity.locations[0].salesforceValue
-  }
-
-  // Check if the site has multiple locations & if Open
-  isMultiple(siteData)
-  
+function getSiteData() {
   return siteData
 }
 
