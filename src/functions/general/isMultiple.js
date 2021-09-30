f84e0acff86d665af3ac1ebcae1c3f09405b9927
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-09-29 21:36:21 -0500

    accepting city as a param

--- src/functions/general/isMultiple.js
+++ src/functions/general/isMultiple.js
@@ -1,12 +1,8 @@
-import getCity from '../../functions/general/getCity'
-
 // This function is called in getSiteData() in Layout Comp
 // 1. Check whether the current city is a single location city or multiple
 // 2. Check if any store is open
 // 3. Update multiple & open props in siteData obj accordingly
-function isMultiple(siteData) {
-  // Get the current site city whether it's single or multiple
-  const city = getCity(siteData) // returns object
+function isMultiple(siteData, city) {
 
   // For cities with multiple locations & at least ONE location is open
   if(city.locations.length > 1) {
