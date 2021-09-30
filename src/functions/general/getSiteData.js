40584243a655ad62172c52d334a84077d8772583
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-09-30 14:47:26 -0500

    Global Milan phone num

--- src/functions/general/getSiteData.js
+++ src/functions/general/getSiteData.js
@@ -1,9 +1,10 @@
 import setSiteData from './setSiteData'
 
-const siteData = setSiteData('Phoenix', 'Arizona')
+export const MILAN_PHONE = '833-667-2967'
+export const NO_RAZOR = '1-833-NO-RAZOR'
 
-function getSiteData() {
-  return siteData
-}
+const siteData = setSiteData('Houston', 'Texas')
+
+function getSiteData() { return siteData }
 
 export default getSiteData
