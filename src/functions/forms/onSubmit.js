78ae92d1cbc9f79102bf68fd4a1affaf72c6d5ee
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-09-29 21:40:39 -0500

    url renamed to be origin

--- src/functions/forms/onSubmit.js
+++ src/functions/forms/onSubmit.js
@@ -1,9 +1,9 @@
 function onSubmit(formState, siteData, formRef, succesRef) {
   if(formState.include.action === 'question') {
-    succesRef.current.value = siteData.url + 'form/processing.html'
+    succesRef.current.value = siteData.origin + 'form/processing.html'
   }
   if(formState.include.action === 'self_schedule') {
-    succesRef.current.value = `https://milanlaser.force.com/s/?firstname=${formState.user.first_name}&lastname=${formState.user.last_name}&phone=${formState.user.phone.slice(3)}&email=${formState.user.email}&zip=${formState.store.zipcode}&success_url=${siteData.url}form/self-success-force.html`
+    succesRef.current.value = `https://milanlaser.force.com/s/?firstname=${formState.user.first_name}&lastname=${formState.user.last_name}&phone=${formState.user.phone.slice(3)}&email=${formState.user.email}&zip=${formState.store.zipcode}&success_url=${siteData.origin}form/self-success-force.html`
   }
   console.log(formState)
   // formRef.current.submit()
