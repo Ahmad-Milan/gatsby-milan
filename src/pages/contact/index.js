a96ab8ea00e132c593a49e9ef200514b078b4d1f
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-09-30 14:47:03 -0500

    nearby locations phone numbers added

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import Layout, {siteData, city} from '../../components/layout/Layout'
 import Contact from '../../components/forms/contact/Contact'
+import getNearbyLocations from '../../functions/general/getNearbyLocations'
+import { MILAN_PHONE, NO_RAZOR } from '../../functions/general/getSiteData'
 
 import './contact.css'
+
+const nearbyLocations = getNearbyLocations(siteData)
 
 function index() {
   return (
@@ -15,13 +19,38 @@ function index() {
             <div className="col-11 col-md-8 col-lg-6 text-all-bg shadow-sm rounded mx-auto">
               <div className="text-center">
                 <h2 className="mt-3 pb-3">Milan Laser | {siteData.city}, {siteData.stateShort}</h2>
-                <h5 className="ribbon">Call Us <a href="tel:833-667-2967" className="text-white stretched-link">1-833-NO-RAZOR</a></h5>
+                <h5 className="ribbon">Call Us <a href={`tel:${MILAN_PHONE}`} className="text-white stretched-link">{NO_RAZOR}</a></h5>
                 {/* <a name="askQ" id="askQ" ></a> */}
-                <h5 className="mt-4 mb-3">To Re-schedule a Treatment <a href="tel:518-223-9045" className="main-blue d-block d-sm-inline">518-223-9045</a></h5>
-                <h5>Or Send Us a Question</h5>
+                <h5 className="mt-4 mb-2">To Re-schedule a Treatment
+                {
+                  nearbyLocations.length === 1 && 
+                  <span>:&nbsp;
+                    <a href={`tel:${nearbyLocations[0].phone !== '' ? nearbyLocations[0].phone : MILAN_PHONE}`} className="main-blue d-block d-md-inline">
+                    {`${nearbyLocations[0].phone !== '' ? nearbyLocations[0].phone : MILAN_PHONE}`}
+                    </a>
+                  </span>
+                }
+                </h5>
+                {
+                  nearbyLocations.length > 1 &&
+                  <ul className="row text-lg-start">
+                    {
+                      nearbyLocations.map((store, x) => (
+                      <li key={x} className="col-lg-6 text-center">
+                        <span className="h6">{store.location}:&nbsp;
+                          <a href={`tel:${store.phone !== '' ? store.phone : MILAN_PHONE}`} className="main-blue">
+                          {`${store.phone !== '' ? store.phone : MILAN_PHONE}`}
+                          </a>
+                        </span>
+                      </li>
+                      ))
+                    }
+                  </ul>
+                }
               </div>
-              
-              
+
+              <h5 className="text-center mt-3">Or Send Us a Question</h5>
+              <Contact siteData={siteData} city={city} />
             </div>
           </div>
         </div>
