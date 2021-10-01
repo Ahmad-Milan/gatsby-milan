d473d5bfcb3f8a51c20e7c9056df8a327acb7b1c
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-09-30 19:28:44 -0500

    moved to quote folder

--- src/components/forms/quote/quoteSchema.js
+++ src/components/forms/quote/quoteSchema.js
@@ -2,7 +2,7 @@ import * as Yup from 'yup'
 
 function quoteSchema() {
   const schema = Yup.object().shape({
-    first_name: Yup.string().required('First name is required!'),
+    first_name: Yup.string().required('This field is required!'),
     email: Yup.string().email('Enter a valid email!').required('Email is required!'),
     phone: Yup.string().min(15, 'Enter valid phone number!').required('Phone number is required!')
   })
