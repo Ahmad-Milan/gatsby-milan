1b40921f98a36055f1847cadbe2a5f99bd824a8f
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-10-05 13:52:24 -0500

    MILAN_PHONE removed

--- src/functions/general/getSiteData.js
+++ src/functions/general/getSiteData.js
@@ -1,9 +1,6 @@
 import setSiteData from './setSiteData'
 
-export const MILAN_PHONE = '833-667-2967'
-export const NO_RAZOR = '1-833-NO-RAZOR'
-
-const siteData = setSiteData('Houston', 'Texas')
+const siteData = setSiteData('Phoenix', 'Arizona')
 
 function getSiteData() { return siteData }
 
