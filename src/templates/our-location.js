436918a24b711aa69a7e765857d85514f4d238d2
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-10-05 11:35:50 -0500

    file created

---
+++ src/templates/our-location.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../components/layout/Layout'
+
+function OurLocation() {
+  return (
+    <Layout>
+      <div className="full-section">
+        <h1>title</h1>
+      </div>
+
+      <div dangerouslySetInnerHTML={} />
+      
+    </Layout>
+  )
+}
+
+export default OurLocation
