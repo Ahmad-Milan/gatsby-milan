c42da1ee2cdc619f37eb6162127bafe9b598350b
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-10-11 15:51:01 -0500

    div parent added to ul

--- src/components/header/navbar/Navbar.js
+++ src/components/header/navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import Link  from "../../../functions/general/linkTesting"
 import SubMenu from './subMenu/SubMenu'
-import { FaCaretDown, FaCaretUp, FaTimes } from 'react-icons/fa'
+import { FaCaretDown, FaCaretUp, FaTimes, FaPhoneAlt } from 'react-icons/fa'
 import updateMenuExpansion from './updateMenuExpansion'
 
 function Navbar({ menu, navbarSize, isOpen, closeMenu }) {
@@ -15,60 +15,69 @@ function Navbar({ menu, navbarSize, isOpen, closeMenu }) {
   }
 
   return (
-    <ul className={`navbar__menu ${navbarSize} ${isOpen}`}>
-      { // X close button for smaller screens
-        navbarSize === 'navbar__menu--sm' && 
-        <li className="menu__item" onClick={closeMenu}>
-          <div className="pb-1 pe-4 text-end close_menu_icon">
-            <span><FaTimes /></span>
-          </div>
-        </li>
-      }
-
-    {menu.map((menuItem, i) =>  (
-      <li key={i} className="menu__item">
-        <div className="menu__item__link__wrapper">
-          {/*👇 Main menu links go here */}
-          {/* Home / Specials / The Process / Areas / Contact & About Us  */}
-          <Link
-            to={menuItem.pathname} 
-            activeClassName="active-link"
-            partiallyActive={menuItem.pathname === "/" ? false : true }
-            className="text-uppercase"
-            onClick={closeMenu}
-            >
-              {menuItem.link}
-          </Link>
-          { menuItem?.subMenu && 
-          <>
-            {
-              navbarSize === 'navbar__menu--lg' && 
-              <span className="caret__icon caret__icon--down"><FaCaretDown /></span>
-            }
-            {
-              navbarSize === 'navbar__menu--sm' && 
-              <span className="caret__icon" onClick={() => handleClick(menuItem)}>
-                { menuItem.expanded ? <FaCaretUp /> : <FaCaretDown /> }
-              </span> 
-            }
-          </>
-          }
-        </div>
-        {
-          menuItem?.subMenu && 
-          <>
-            {/* menuItem ex: Sepcials -> subMenuItems : {This Month's Specials, Payment Plan} */} {/* Returns <ul></ul> */}
-            <SubMenu 
-              menu={menu} menuItem={menuItem}
-              navbarSize={navbarSize} index={i}
-              expanded={menuItem.expanded} 
-              closeMenu={closeMenu}/>
-          </>
+    <div className={`navbar__menu ${navbarSize} ${isOpen}`}>
+      <ul className="mb-0">
+        { // X close button for smaller screens
+          navbarSize === 'navbar__menu--sm' && 
+          <li className="menu__item" onClick={closeMenu}>
+            <div className="pb-1 pe-4 text-end close_menu_icon">
+              <span><FaTimes /></span>
+            </div>
+          </li>
         }
-      </li>
-      )
-    )}
-    </ul>
+
+        {
+          menu.map((menuItem, i) =>  (
+          <li key={i} className="menu__item">
+            <div className="menu__item__link__wrapper">
+              {/*👇 Main menu links go here */}
+              {/* Home / Specials / The Process / Areas / Contact & About Us  */}
+              <Link
+                to={menuItem.pathname} 
+                activeClassName="active-link"
+                partiallyActive={menuItem.pathname === "/" ? false : true }
+                className="text-uppercase"
+                onClick={closeMenu}
+                >
+                  {menuItem.link}
+              </Link>
+              { menuItem?.subMenu && 
+              <>
+                {
+                  navbarSize === 'navbar__menu--lg' && 
+                  <span className="caret__icon caret__icon--down"><FaCaretDown /></span>
+                }
+                {
+                  navbarSize === 'navbar__menu--sm' && 
+                  <span className="caret__icon" onClick={() => handleClick(menuItem)}>
+                    { menuItem.expanded ? <FaCaretUp /> : <FaCaretDown /> }
+                  </span> 
+                }
+              </>
+              }
+            </div>
+            {
+              menuItem?.subMenu && 
+              <>
+                {/* menuItem ex: Sepcials -> subMenuItems : {This Month's Specials, Payment Plan} */} {/* Returns <ul></ul> */}
+                <SubMenu 
+                  menu={menu} menuItem={menuItem}
+                  navbarSize={navbarSize} index={i}
+                  expanded={menuItem.expanded} 
+                  closeMenu={closeMenu}/>
+              </>
+            }
+          </li>
+          )
+        )}
+      </ul>
+
+      <center className="header-tel d-sm-none pt-5 pb-3">
+        <a href="tel:833-667-2967">
+          <i><FaPhoneAlt /></i><span className="ps-2">1-833-NO-RAZOR</span>
+        </a>
+      </center>
+    </div>
   )
 }
 
