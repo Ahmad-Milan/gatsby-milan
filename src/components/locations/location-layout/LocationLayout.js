c792615a16e2acfafa923e46fdc16043c99c040f
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-10-13 20:13:04 -0500

    defaultImages obj added

--- src/components/locations/location-layout/LocationLayout.js
+++ src/components/locations/location-layout/LocationLayout.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import TempHero from './TempHero'
 
+const sharedPath = 'https://milanlaser.com/gatsby/images/shared/'
+
+// To be used if the store doesn't have pro images yet
+const defaultImages = {
+  exterior: `${sharedPath}coming-soon-cam.jpg`,
+  thumbnail: `${sharedPath}coming-soon-cam-sm.jpg`,
+  frontdoor: `${sharedPath}FrontDoor.jpg`,
+  frontdesk: `${sharedPath}FrontDesk.jpg`,
+  lobby: `${sharedPath}Lobby.jpg`,
+  txroom: `${sharedPath}TXRoom.jpg`,
+  consultroom: `${sharedPath}ConsultationRoom.jpg`,
+}
+
 function Layout({siteData, city, store}) {
+  console.log('Layout')
   return (
     <>
-      <TempHero siteData={siteData} city={city} store={store} />
+      <TempHero siteData={siteData} city={city} store={store} defaultImages={defaultImages} />
     </>
   )
 }
