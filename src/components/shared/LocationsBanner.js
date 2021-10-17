7d5b1fb59f72ba4e65f854f824f0bfa196475a84
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-10-17 12:43:37 -0500

    LocationsBanner comp created

---
+++ src/components/shared/LocationsBanner.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function LocationsBanner() {
+  return (
+    <section className="w-100 py-4 locations-banner">
+      <div className="container text-center">
+        <a href="https://milanlaser.com/locations/" target="_blank">
+          <h2 className="text-center text-white subhead-sm text-capitalize">
+            <span className="location-number">100</span>+ Milan Laser Locations
+          </h2>
+          <img src="https://milanlaser.com/images/banner-button.png" alt="" />
+        </a>
+      </div>
+    </section>
+  )
+}
+
+export default LocationsBanner
