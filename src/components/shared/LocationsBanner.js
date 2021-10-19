68ee29c09fad8e7d887a72d381794b7737b2f306
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-10-18 19:57:36 -0500

    rel="noreferrer" added

--- src/components/shared/LocationsBanner.js
+++ src/components/shared/LocationsBanner.js
@@ -4,7 +4,7 @@ function LocationsBanner() {
   return (
     <section className="w-100 py-4 locations-banner">
       <div className="container text-center">
-        <a href="https://milanlaser.com/locations/" target="_blank">
+        <a href="https://milanlaser.com/locations/" target="_blank" rel="noreferrer">
           <h2 className="text-center text-white subhead-sm text-capitalize">
             <span className="location-number">100</span>+ Milan Laser Locations
           </h2>
