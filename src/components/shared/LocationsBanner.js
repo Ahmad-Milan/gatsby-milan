67653c5322cef77df20482cf7e0409c9909616d4
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-10-20 06:29:46 -0500

    Countup & visbility sensor added

--- src/components/shared/LocationsBanner.js
+++ src/components/shared/LocationsBanner.js
@@ -1,16 +1,24 @@
 import React from 'react'
+import openStoresDisplayed from '../../functions/general/openStoresDisplayed'
+import CountUp from 'react-countup'
+import VisibilitySensor from 'react-visibility-sensor'
 
 function LocationsBanner() {
+  const openStoresNum = openStoresDisplayed()
   return (
     <section className="w-100 py-4 locations-banner">
-      <div className="container text-center">
-        <a href="https://milanlaser.com/locations/" target="_blank" rel="noreferrer">
-          <h2 className="text-center text-white subhead-sm text-capitalize">
-            <span className="location-number">100</span>+ Milan Laser Locations
-          </h2>
-          <img src="https://milanlaser.com/images/banner-button.png" alt="" />
-        </a>
-      </div>
+      <VisibilitySensor partialVisibility offset={{ bottom: 200 }}>
+      {({ isVisible }) => (
+        <div className="container text-center">
+          <a href="https://milanlaser.com/locations/" target="_blank" rel="noreferrer">
+            <h2 className="text-center text-white subhead-sm text-capitalize">
+              {isVisible ? <CountUp className="text-white" end={openStoresNum} duration={2} /> : null}+ Milan Laser Locations
+            </h2>
+            <img src="https://milanlaser.com/images/banner-button.png" alt="" />
+          </a>
+        </div>
+      )}
+      </VisibilitySensor>
     </section>
   )
 }
