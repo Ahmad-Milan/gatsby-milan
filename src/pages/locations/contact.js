7c98d70ae37e0c19adf034635025e1d9928cfd7c
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-10-20 21:15:29 -0500

    SocialMedia comp added

--- src/pages/locations/contact.js
+++ src/pages/locations/contact.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout, {siteData, city} from '../../components/layout/Layout'
 import ContactHero from '../../components/locations/contact/ContactHero'
+import SocialMedia from '../../components/locations/contact/SocialMedia'
 import './styles.css'
 
 function ContactPage() {
@@ -8,6 +9,8 @@ function ContactPage() {
   return (
     <Layout>
       <ContactHero siteData={siteData} city={city} />
+      <span id="facebook"></span>
+      <SocialMedia city={city} />
     </Layout>
 
   )
