8c0b719ec1e94426bfaa9bb6f09d9aad81a9ff66
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-10-23 09:54:49 -0500

    FaqsHero comp created

---
+++ src/components/process/faqs/FaqsHero.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles.css'
+
+function FaqsHero() {
+  return (
+    <section className="full-section hero faqs-hero">
+      <div className="container px-3 text-left text-md-center text-lg-start">
+        <h1 className="my-3 my-sm-5">FAQs About <br className="d-lg-none" /><span className="smaller">Laser Hair Removal</span></h1>
+        <h2 className="h4 pt-3 pt-sm-0 pr-5 pr-sm-0">Get answers about cost &amp; 
+          <br className="d-none d-sm-inline d-lg-none" /> effectiveness of laser hair removal.
+        </h2>
+      </div>
+    </section>
+  )
+}
+
+export default FaqsHero
