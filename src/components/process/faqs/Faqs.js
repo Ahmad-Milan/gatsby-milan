6b366b815758591c19961b21acb7eac28af8335c
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-10-23 09:54:34 -0500

    Faqs comp created

---
+++ src/components/process/faqs/Faqs.js
@@ -0,0 +1,50 @@
+import React, {useContext} from 'react'
+import { Accordion, Card } from 'react-bootstrap'
+import { useAccordionButton } from 'react-bootstrap/AccordionButton'
+import AccordionContext from 'react-bootstrap/AccordionContext'
+
+function ContextAwareToggle({ children, eventKey, callback }) {
+  const { activeEventKey } = useContext(AccordionContext);
+
+  const decoratedOnClick = useAccordionButton(
+    eventKey,
+    () => callback && callback(eventKey),
+  );
+
+  const isCurrentEventKey = activeEventKey === eventKey;
+
+  return (
+    <button
+      type="button"
+      style={{ backgroundColor: isCurrentEventKey ? 'pink' : 'lavender' }}
+      onClick={decoratedOnClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+function Faqs() {
+  return (
+    <Accordion>
+      <Card>
+        <Card.Header>
+          <ContextAwareToggle eventKey="0">Click me!</ContextAwareToggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body>Hello! I'm the body</Card.Body>
+        </Accordion.Collapse>
+      </Card>
+      <Card>
+        <Card.Header>
+          <ContextAwareToggle eventKey="1">Click me!</ContextAwareToggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="1">
+          <Card.Body>Hello! I'm another body</Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
+  );
+}
+
+export default Faqs
