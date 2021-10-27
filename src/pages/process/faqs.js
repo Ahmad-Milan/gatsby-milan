440297bddffd82a95a115f40cde8c2051f799a0e
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-10-26 21:05:52 -0500

    AskNewQuestion Comp Added

--- src/pages/process/faqs.js
+++ src/pages/process/faqs.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from '../../components/layout/Layout'
+import AskNewQuestion from '../../components/process/faqs/AskNewQuestion'
 import Faqs from '../../components/process/faqs/Faqs'
 import FaqsHero from '../../components/process/faqs/FaqsHero'
 import Divider from '../../components/shared/Divider'
@@ -10,6 +11,7 @@ function FaqsPage() {
       <FaqsHero />
       <Divider />
       <Faqs />
+      <AskNewQuestion />
     </Layout>
   )
 }
