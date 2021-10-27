9056e59aea905fc5ccf9d60cc0841a7d75e5c2ce
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-10-27 10:29:38 -0500

    minor styling change

--- src/components/forms/shared/SubscriptionBox.js
+++ src/components/forms/shared/SubscriptionBox.js
@@ -4,9 +4,9 @@ function SubscriptionBox({formState, handleSubscription, mailchimpID}) {
   return (
     <>
       <input 
-        className="me-2 form-check-input" type="checkbox" id={mailchimpID}  name="updates" 
+        className="form-check-input mt-0" type="checkbox" id={mailchimpID} name="updates" 
         checked={formState.user.mailchimp} onChange={(event) => handleSubscription(event)}/>
-      <label className="main-blue mailchimp" htmlFor={mailchimpID}><small>Get updates on laser hair removal deals. (no spam)</small></label>
+      <label className="main-blue mailchimp ps-2 lh-sm" htmlFor={mailchimpID}><small>Get updates on laser hair removal deals. (no spam)</small></label>
     </>
   )
 }
