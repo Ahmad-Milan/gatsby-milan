3503ac2d9d3b74235f016709e04b27c3c4c97964
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-10-30 09:22:04 -0500

    siteLocations.locations

--- src/functions/general/updateCurrentCity.js
+++ src/functions/general/updateCurrentCity.js
@@ -1,11 +1,11 @@
-import site_locations from '../../data/site-locations.json'
+import siteLocations from '../../data/site-locations.json'
 
 // This function will combine the site locations props with props coming from site-locations.json
 // IMPORTANT: make sure site-locations array contains all the locations in city.locations array
 // The 2 arrays lengths MUST be the same, otherwise it will throw many errors
 let final_locations = []
 const updateCurrentCity = (city) => {
-  site_locations.forEach(elem => {
+  siteLocations.locations.forEach(elem => {
     let store = city.locations.find(loc => loc.salesforceValue === elem.salesforceValue)
     store = {...elem, ...store}
     final_locations.push(store)
