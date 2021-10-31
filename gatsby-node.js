0fc807375cbc9c8f137a67aaa4872e16ab8d791a
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-10-31 10:41:19 -0500

    create city page programmatically

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,21 @@
 exports.createPages = ({ actions: { createPage } }) => {
   const siteLocations = require("./src/data/site-locations.json")
-  // Create a page for each location.
-  siteLocations.locations.forEach((store) => {
-    createPage({
-      path: `/locations/${siteLocations.city.trim().replace(/\s/g, '').toLowerCase()}/${store.location.trim().toLowerCase().replace(/\s/g, '')}/`,
-      component: require.resolve("./src/components/layout/template.js"),
-      context: { store },
-    })
+  // Create a page for city
+  createPage({
+    path: `/locations/${siteLocations.city.trim().replace(/\s/g, '').toLowerCase()}/`,
+    component: require.resolve("./src/components/templates/city-template.js"),
+    context: { siteLocations },
   })
+
+  // if the city has multiple locations
+  if(siteLocations.locations.length > 1) {
+    // Create a page for each store.
+    siteLocations.locations.forEach((store) => {
+      createPage({
+        path: `/locations/${siteLocations.city.trim().replace(/\s/g, '').toLowerCase()}/${store.location.trim().toLowerCase().replace(/\s/g, '')}/`,
+        component: require.resolve("./src/components/templates/store-template.js"),
+        context: { store },
+      })
+    })
+  }
 }
