21ce67c5b9f2698aea93eec4b6ae3cd0ecdd8d58
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-10-31 20:34:59 -0500

    using facebook_start constant

--- src/components/locations/contact/Facebook.js
+++ src/components/locations/contact/Facebook.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { FaFacebookSquare } from 'react-icons/fa'
+import { facebook_start } from '../../../constants/constants'
 
 function Facebook({location}) {
   return (
     <>
       <a 
         className="d-block main-blue h5 mb-1 p-2 pl-3 me-sm-2 shadow-sm rounded bg-white" 
-        href={`https://www.facebook.com/MilanLaser${location.facebook}`} target="_blank" rel="noreferrer">
+        href={`${facebook_start}${location.reviews.facebook}`} target="_blank" rel="noreferrer">
       <FaFacebookSquare className="me-1 pb-1 fs-3" /> {location.location},&nbsp;{location.stateShort}</a>
     </>
   )
