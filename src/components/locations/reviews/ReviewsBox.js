50f8e7706ed8f88ab4e14f489d94f8793caee49f
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-10-31 20:53:38 -0500

    fixing map return bug

--- src/components/locations/reviews/ReviewsBox.js
+++ src/components/locations/reviews/ReviewsBox.js
@@ -1,16 +1,15 @@
 import React from 'react'
 
 function ReviewsBox({city}) {
+  const stores_with_reviews = city.locations.filter(store => store.reviews.reviews_token !== '')
   return (
     <div className="container">
       <div className="row justify-content-center">
         <div className="reviews-box px-3 py-2 m-2 col-md-10">
           {
-            city.locations.map((location, x) => {
-              if(location.reviews.reviews_token !== '') return(
-                <div key={x} className="pt-3" data-romw-token={location.reviews.reviews_token}></div>
-              )
-            })
+            stores_with_reviews.map((store, x) => (
+              <div key={x} className="pt-3" data-romw-token={store.reviews.reviews_token}></div>
+            ))
           }
         </div>
       </div>
