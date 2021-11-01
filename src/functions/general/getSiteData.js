5323a16fd15a27ea76ffc3268c1d345063b03430
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-10-31 20:37:16 -0500

    passing milan pathname instead of city name

--- src/functions/general/getSiteData.js
+++ src/functions/general/getSiteData.js
@@ -1,7 +1,7 @@
 import setSiteData from './setSiteData'
 import siteLocations from '../../data/site-locations.json' 
 
-const siteData = setSiteData(siteLocations.city, siteLocations.stateShort)
+const siteData = setSiteData(siteLocations.pathname, siteLocations.stateShort)
 
 function getSiteData() { return siteData }
 
