0a485cfab511d4d08923b3634c528431a06af2f0
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-11-01 19:24:16 -0500

    using siteData.json instead of siteLocations

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,18 @@
 exports.createPages = ({ actions: { createPage } }) => {
-  const siteLocations = require("./src/data/site-locations.json")
+  const siteData = require("./src/data/siteData.json")
   // Create a page for city
   createPage({
-    path: `/locations/${siteLocations.city.trim().replace(/\s/g, '').toLowerCase()}/`,
+    path: `/locations/${siteData.city.trim().replace(/\s/g, '').toLowerCase()}/`,
     component: require.resolve("./src/components/templates/city-template.js"),
-    context: { siteLocations },
+    context: { siteData },
   })
 
   // if the city has multiple locations
-  if(siteLocations.locations.length > 1) {
+  if(siteData.locations.length > 1) {
     // Create a page for each store.
-    siteLocations.locations.forEach((store) => {
+    siteData.locations.forEach((store) => {
       createPage({
-        path: `/locations/${siteLocations.city.trim().replace(/\s/g, '').toLowerCase()}/${store.location.trim().toLowerCase().replace(/\s/g, '')}/`,
+        path: `/locations/${siteData.city.trim().replace(/\s/g, '').toLowerCase()}/${store.location.trim().toLowerCase().replace(/\s/g, '')}/`,
         component: require.resolve("./src/components/templates/store-template.js"),
         context: { store },
       })
