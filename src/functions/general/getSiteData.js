852f1593fcfcef8246894be452481952377db76c
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-11-01 19:30:01 -0500

    code refactored

--- src/functions/general/getSiteData.js
+++ src/functions/general/getSiteData.js
@@ -1,8 +1,32 @@
-import setSiteData from './setSiteData'
-import siteLocations from '../../data/site-locations.json' 
+import stores from '../../data/stores.json'
+import siteData from '../../data/siteData.json'
+import combineLocationsProps from './combineLocationsProps'
 
-const siteData = setSiteData(siteLocations.pathname, siteLocations.stateShort)
+// To be called in the Layout Comp
+// Returns siteData obj
+function getSiteData() {
+  // 1. Find the matching state from the stores list
+  let currentState = stores.locations.find(state => state.stateShort === siteData.stateShort)
+  // 2. Find the current city in that state
+  let currentCity = currentState.stores.find(city => city.pathname === siteData.pathname.toLowerCase())
+  // 3. Add more props to siteData obj
+  siteData.origin = `https://milanlaser${siteData.pathname.toLowerCase()}.com/`
+  siteData.state = currentState.state
+  siteData.stateShort = currentState.stateShort
+  siteData.open = currentCity.locations.some(location => location.open === true) // returns Boolean
+  siteData.multiple = currentCity.locations.length > 1 ? true : false
 
-function getSiteData() { return siteData }
+  combineLocationsProps(siteData, currentCity)
+
+  /**************************************/
+  // if the city is a single location city, add a salesforceValue prop
+  if(currentCity.locations.length === 1) {
+    siteData.salesforceValue = currentCity.locations[0].salesforceValue
+  }
+  /**************************************/
+
+  console.log(siteData)
+  return siteData
+}
 
 export default getSiteData
