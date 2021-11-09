a3770e84efd2bd4619cad0c7beae6d97d9dead80
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-11-08 19:49:25 -0600

    minor change

--- src/pages/locations/contact.js
+++ src/pages/locations/contact.js
@@ -2,17 +2,14 @@ import React from 'react'
 import Layout, {siteData} from '../../components/templates/Layout'
 import ContactHero from '../../components/locations/contact/ContactHero'
 import SocialMedia from '../../components/locations/contact/SocialMedia'
-import './styles.css'
 
 function ContactPage() {
-
   return (
     <Layout>
       <ContactHero siteData={siteData}/>
       <span id="facebook"></span>
       <SocialMedia siteData={siteData} />
     </Layout>
-
   )
 }
 
