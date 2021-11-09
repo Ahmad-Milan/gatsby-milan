8fa132cef84d18cd0f3b0f814f5a18a116b66e78
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-11-08 19:49:57 -0600

    SpecialsHero comp added

--- src/pages/specials/index.js
+++ src/pages/specials/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Layout from '../../components/templates/Layout'
+import Layout, {siteData}  from '../../components/templates/Layout'
 import { Link } from 'gatsby'
+import SpecialsHero from '../../components/specials/SpecialsHero'
 
 function SpecialsPage() {
   return (
     <Layout>
-      <div className="mt-5 pt-5"></div>
-      <h1>Specials Page</h1>
+      <SpecialsHero siteData={siteData} />
       
       <Link to="/specials/estimate/">Customm Quote</Link>
     </Layout>
