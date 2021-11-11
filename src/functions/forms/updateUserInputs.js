2a9a356eb09471faae482c0c965535e83d330aa2
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-11-11 11:05:40 -0600

    checking if user.firs_name exists

--- src/functions/forms/updateUserInputs.js
+++ src/functions/forms/updateUserInputs.js
@@ -2,7 +2,7 @@ function updateUserInputs(formState, user) {
   const updatedFormState = { ...formState } // Shallow clone of formState
 
   // Update formState user values: get the values from formikProps.values
-  updatedFormState.user.first_name = user.first_name
+  if(user.first_name) updatedFormState.user.first_name = user.first_name
   if(user.last_name) updatedFormState.user.last_name = user.last_name
   updatedFormState.user.email = user.email
   if(user.phone) updatedFormState.user.phone = user.phone
