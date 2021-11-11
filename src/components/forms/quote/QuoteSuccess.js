b7dccf753314a7825f0129e1f1ea9cdb44faf120
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-11-10 22:20:26 -0600

    QuoteSuccess comp created

---
+++ src/components/forms/quote/QuoteSuccess.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function QuoteSuccess() {
+  return (
+    <center className="px-3 mt-3 success">
+      <p className="h1 text-success">SUCCESS!</p>
+      <p className="h3 mt-5 mb-3">Your request has been submitted.</p>
+      <p className="h5">We will be contacting you shortly with more information. During normal business hours you can expect to hear from us in about 15 minutes.</p>
+    </center>
+  )
+}
+
+export default QuoteSuccess
