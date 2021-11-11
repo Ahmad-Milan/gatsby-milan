290525b494e10375eb75f85edc2d0fc6c11f2275
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-11-11 11:05:57 -0600

    SignUpModal comp added

--- src/pages/specials/index.js
+++ src/pages/specials/index.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Layout, {siteData}  from '../../components/templates/Layout'
 import { Link } from 'gatsby'
 import SpecialsHero from '../../components/specials/SpecialsHero'
+import SignUpModal from '../../components/modals/SignUpModal'
 
 function SpecialsPage() {
+  const [showSignUpModal, setShowSignUpModal] = useState(false)
+
   return (
     <Layout>
-      <SpecialsHero siteData={siteData} />
-      
+      <SpecialsHero siteData={siteData} setShowSignUpModal={setShowSignUpModal} />
+      {
+        showSignUpModal &&
+        <SignUpModal showSignUpModal={showSignUpModal} setShowSignUpModal={setShowSignUpModal} />
+      }
       <Link to="/specials/estimate/">Customm Quote</Link>
     </Layout>
   )
