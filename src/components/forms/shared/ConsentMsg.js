9e847ffda0b51b3861ffe4c4838165357e824b08
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-11-10 22:20:44 -0600

    ConsentMsg comp created

---
+++ src/components/forms/shared/ConsentMsg.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ConsentMsg() {
+  return (
+    <>
+      I consent by electronic signature to be contacted by Milan Laser by live agent, email &amp; automatic telephone dialer for information, offers or advertisements via email/ phone call/ text message at the number &amp; email provided. No purchase necessary.
+    </>
+  )
+}
+
+export default ConsentMsg
