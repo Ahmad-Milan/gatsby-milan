4ce980dd1ec10fca12c61bd540c8c35a7908b092
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-11-12 12:13:29 -0600

    ProcessHype comp created

---
+++ src/components/process/home/ProcessHype.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles.css'
+
+function ProcessHype() {
+  return (
+    <div className="w-100 mb-n2">
+      <iframe className="porcess-hype" src="https://milanlaser.com/gatsby/proccess_hype.html" title="Laser hair removal process"></iframe>
+    </div>
+  )
+}
+
+export default ProcessHype
