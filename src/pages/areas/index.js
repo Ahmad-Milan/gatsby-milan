20dd8eb9ceede96f56418edacf21242ad27adb8c
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-11-11 18:09:45 -0600

    MilanDealsSection comp added

--- src/pages/areas/index.js
+++ src/pages/areas/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Layout from '../../components/templates/Layout'
+import MilanDealsSection from '../../components/areas/MilanDealsSection'
 
 function AreasPage() {
   return (
-    <div>
-      Body areas main page
-    </div>
+    <Layout>
+      <div className="full-section mt-5">
+        <MilanDealsSection />
+      </div>
+    </Layout>
   )
 }
 
