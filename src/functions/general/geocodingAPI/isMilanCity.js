ea78357b2857aa4d582992285fa6956a9cffb3a1
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-11-16 08:38:51 -0600

    deprecated

--- src/functions/general/geocodingAPI/isMilanCity.js
+++
@@ -1,8 +0,0 @@
-const isMilanCity = (detectedCity, detectedState) => {
-  let filteredCity = detectedState.stores.find(city => {
-    if(city.city === detectedCity) return true
-    else return city.locations.find(location => location.location === detectedCity)
-  })
-  return filteredCity
-}
-export default isMilanCity
