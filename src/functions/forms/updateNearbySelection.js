b8b8aca9d8a39f6bf2999dabe86d26ca37258fba
ahmad-saleh <ahmad.saleh@milanlaser.com>
2021-11-17 23:03:48 -0600

    update city prop in formData & fromState

--- src/functions/forms/updateNearbySelection.js
+++ src/functions/forms/updateNearbySelection.js
@@ -1,5 +1,6 @@
 import scrollTo from 'gatsby-plugin-smoothscroll'
 import updateStoreProps from './updateStoreProps'
+import formData from '../../data/formData.json'
 
 function updateNearbySelection(store, formState, siteData) {
   const updatedFormState = { ...formState } // Shallow clone of formState
@@ -10,6 +11,11 @@ function updateNearbySelection(store, formState, siteData) {
   siteData.locations.map(store => store.selected = false)
   // Then make the clicked one selected
   store.selected = true
+
+  // Update formState and formData city prop
+  updatedFormState.city.cityName = siteData.cityName
+  updatedFormState.city.pathname = siteData.pathname
+  formData.city = {...updatedFormState.city}
 
   scrollTo('#locations-dropdown')
 
